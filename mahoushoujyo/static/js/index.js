ae42795ceb7ce56d1cad7f52d2b514ec968a2728
ToooAir <33440687+ToooAir@users.noreply.github.com>
2018-06-11 03:59:29 +0800

    LOL

--- mahoushoujyo/static/js/index.js
+++ mahoushoujyo/static/js/index.js
@@ -68,3 +68,12 @@
         window.location="register";
       })
   });
+
+  var audio = document.createElement("audio");
+  audio.autoplay = true;
+  audio.load();
+  audio.addEventListener("load",function(){
+    audio.currentTime = 0;
+    audio.play();
+  },true);
+  audio.src = "{%static 'music/index.mp3' %}";
