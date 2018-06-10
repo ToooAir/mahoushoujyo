5af63f9a9b7111e23210bc7786a4424deee7c45a
ToooAir <33440687+ToooAir@users.noreply.github.com>
2018-06-11 01:42:07 +0800

    LOL

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'hello',
     'jerry',
-    'sitela'
+    'sitela',
+    'mahoushoujyo'
 ]
 
 MIDDLEWARE = [
@@ -111,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Taipei'
 
 USE_I18N = True
 
