b339821080997234311b010a76a6ed0e3722a995
ToooAir <33440687+ToooAir@users.noreply.github.com>
2018-06-11 09:40:50 +0800

    LOL

--- mahoushoujyo/views.py
+++ mahoushoujyo/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render,redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate, login	,logout
 from django.contrib.auth.models import User 
-# import msvcrt
+from django.http import HttpResponse
+import msvcrt
 # Create your views here.
 def index(request):
 	return render(request,'index.html')
@@ -10,20 +11,22 @@ def signin(request):
 	if request.method == 'POST':
 		account = request.POST.get('account')
 		password = request.POST.get('password')
-		user = authenticate(username=account, password=password)
-		if user is not None:
-			login(request, user)
-			return redirect('main')
+		if User.objects.filter(username=account).exists():
+			user = authenticate(username=account, password=password)
+			if user is not None:
+				login(request,user)
+				return redirect('main')
+			else:
+				return HttpResponse("<script>alert('Password is error');window.location.replace(window.location.href);</script>")
 		else:
-			return redirect('signin') #question
+			return HttpResponse("<script>alert('This account does not exist');window.location.replace(window.location.href);</script>")
 	return render(request,'signin.html')
 def register(request):
 	if request.method == 'POST':
 		account = request.POST.get('account')
-		password = request.POST.get('password')
 		password2 = request.POST.get('password2')
-		if password2 != password :
-			messages.error(request,'密碼確認錯誤')
+		if User.objects.filter(username=account).exists() :
+			return HttpResponse("<script>alert('The account already exists');window.location.replace(window.location.href);</script>")
 		else:
 			User.objects.create_user(account,"",password2)
 			return redirect('index')
