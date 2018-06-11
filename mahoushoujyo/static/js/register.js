49782211c8dd9bbbac82408aca8801b165a5e7f7
ToooAir <33440687+ToooAir@users.noreply.github.com>
2018-06-11 10:07:14 +0800

    LOL

--- mahoushoujyo/static/js/register.js
+++ mahoushoujyo/static/js/register.js
@@ -8,8 +8,12 @@ $('#submit').click(function(){
 			var password = $("#apassword").val();
 			var password2 =$("#bpassword").val();
 			if(password != password2){
+				$("#form").attr('target', 'nm_iframe');
 				alert('Password is not correct');
+				$("#apassword").val("");
+				$("#bpassword").val("");
 			}else{
+				$("#form").attr('target','');
 				$('#form').submit();
 			}
 		});
