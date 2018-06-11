5cf6c7533eb0d6387029018126a12025b2eb249a
ToooAir <33440687+ToooAir@users.noreply.github.com>
2018-06-11 10:11:53 +0800

    LOL

--- mahoushoujyo/views.py
+++ mahoushoujyo/views.py
@@ -3,7 +3,6 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login	,logout
 from django.contrib.auth.models import User 
 from django.http import HttpResponse
-import msvcrt
 # Create your views here.
 def index(request):
 	return render(request,'index.html')
